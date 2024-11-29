7b68bdf3b201e005caeea6b095a03381298aeaad
Ryan H. <Ryanhackbart@gmail.com>
2024-11-29 10:56:50 -0500

    predict()

--- classifier.cpp
+++ classifier.cpp
@@ -1,11 +1,20 @@
+#include <stdio.h>
 
 void train(void);
+void predict(void);
 
 int main(void) {
 
 	train(void);
+	predict(void);
+
+	return 0;
 }
 
 void train(void) {
 	printf("train");
+}
+
+void predict(void) {
+	printf("predict")
 }
